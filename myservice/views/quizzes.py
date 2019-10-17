02efd4b66675167f25ca3515c4b969b90212e1ef
Eleonora <eleonora.digregorio8@gmail.com>
2019-10-17 20:27:39 +0200

    final commit

--- myservice/views/quizzes.py
+++ myservice/views/quizzes.py
@@ -6,53 +6,50 @@ quizzes = JsonBlueprint('quizzes', __name__)
 
 _LOADED_QUIZZES = {}  # list of available quizzes
 _QUIZNUMBER = 0  # index of the last created quizzes
-_QUIZTOT = 0  # total number of quizzes
 
 
-# TODO: complete the decoration
 @quizzes.route("/quizzes", methods=['GET', 'POST'])
 def all_quizzes():
     if 'POST' == request.method:
-        # TODO: check if the request structure is correct
-        result = create_quiz(request)
-        global _QUIZTOT
-        _QUIZTOT += 1
+        # Create new quiz
+        try:
+            result = create_quiz(request)
+        except:
+            # error 422: Unprocessable Entity, i.e. well-formed json but semantic errors
+            abort(422)
     elif 'GET' == request.method:
+        # Retrieve all loaded quizzes
         result = get_all_quizzes(request)
     return result
 
-# TODO: complete the decoration
+
 @quizzes.route("/quizzes/loaded", methods=['GET'])
 def loaded_quizzes():  # returns the number of quizzes currently loaded in the system
-    return {"loaded_quizzes":_QUIZTOT}
+    return {"loaded_quizzes": len(_LOADED_QUIZZES)}
 
 
-# TODO: complete the decoration
 @quizzes.route("/quiz/<id>", methods=['GET', 'DELETE'])
 def single_quiz(id):
     global _LOADED_QUIZZES
-    global _QUIZTOT
     result = ""
     check_id_type(id)
     exists_quiz(id)
 
     if 'GET' == request.method:
+        # retrieve a quiz <id>
         result = _LOADED_QUIZZES[id].serialize()
     elif 'DELETE' == request.method:
-        # TODO: delete a quiz and get back number of answered questions
+        # delete a quiz and get back number of answered questions
         # and total number of questions
+        deleted_item = _LOADED_QUIZZES.pop(id)
         result = {
-            # controlla se -1
-            "answered_questions": _LOADED_QUIZZES[id].currentQuestion,
-            "total_questions": len(_LOADED_QUIZZES[id].questions)
+            "answered_questions": deleted_item.currentQuestion,
+            "total_questions": len(deleted_item.questions)
         }
-        _LOADED_QUIZZES.pop(id)
-        _QUIZTOT -= 1
 
     return result
 
 
-# TODO: complete the decoration
 @quizzes.route("/quiz/<id>/question", methods=['GET'])
 def play_quiz(id):
     global _LOADED_QUIZZES
@@ -62,18 +59,17 @@ def play_quiz(id):
     exists_quiz(id)
 
     if 'GET' == request.method:
-        # TODO: retrieve next question in a quiz, handle exceptions
+        # retrieve next question in a quiz, handle exceptions
         try:
             result = _LOADED_QUIZZES[id].getQuestion()
-        except CompletedQuizError :
-            result = jsonify({'msg': "completed quiz"}) 
-        except LostQuizError :
+        except CompletedQuizError:
+            result = jsonify({'msg': "completed quiz"})
+        except LostQuizError:
             result = jsonify({'msg': "you lost!"})
 
     return result
 
 
-# TODO: complete the decoration
 @quizzes.route("/quiz/<id>/question/<answer>", methods=['PUT'])
 def answer_question(id, answer):
     global _LOADED_QUIZZES
@@ -82,33 +78,26 @@ def answer_question(id, answer):
     exists_quiz(id)
     quiz = _LOADED_QUIZZES[id]
 
-    # TODO: check if quiz is lost or completed and act consequently
+    # check if quiz is lost or completed and act consequently
     if(quiz.isCompleted()):
         result = 'completed quiz'
     elif(quiz.isLost()):
         result = 'you lost!'
     elif 'PUT' == request.method:
+        # Check answers and handle exceptions
         try:
             result = quiz.checkAnswer(answer)
-        except CompletedQuizError :
+        except CompletedQuizError:
             result = "you won 1 million clams!"
         except LostQuizError:
             result = "you lost!"
         except NonExistingAnswerError:
             result = "non-existing answer!"
-
-    # TODO: Check answers and handle exceptions
-
     return jsonify({'msg': result})
 
 ############################################
 # NEW FUNCTIONS BELOW
 ############################################
-
-
-# def check_quiz_post(request):
-    # if key doesn't exist, returns a 400, bad request error
-#    qs = request.args['questions']
 
 
 def check_id_type(id):
